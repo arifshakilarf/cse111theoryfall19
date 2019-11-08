bad70ede3133d9ec51a4f0e130cd07548633ddab
19101131 <55811016+19101131@users.noreply.github.com>
2019-11-08 23:01:40 +0600

    Add files via upload

---
+++ MyString.java
@@ -0,0 +1,16 @@
+public class MyString{
+  char [] c;
+  
+  /*This is the constructor
+   */
+  public MyString(String s){
+    c = s.toCharArray();
+  }
+  
+  public int length (){
+    //Write your code here!
+    return -1;
+    
+  }
+
+}
---
+++ Tester.java
@@ -0,0 +1,8 @@
+public class Tester{
+	 public static void main (String[] args){
+		 
+		MyString s1 = new MyString("ABCDE");
+		System.out.println(s1.length());// This should print 5
+		
+	  }
+}
